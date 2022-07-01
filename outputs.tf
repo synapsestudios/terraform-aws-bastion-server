732885d803fb3440324e4014ebeb1486bf9df6d0
Andrew Reida <andrew@synapsestudios.com>
2022-07-01 10:53:41 -0700

    terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "public_ip" {
   description = "The public IP address associated with this Bastion server"
-  value = aws_eip.lb.public_ip
+  value       = aws_eip.lb.public_ip
 }
 
 # Output the secret name for the ssh key
 output "ssh_key_name" {
   description = "The name of the Secrets Manager key for the bastion server's ssh key"
-  value = aws_secretsmanager_secret.this.name
+  value       = aws_secretsmanager_secret.this.name
 }
